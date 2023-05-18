45e61721a6b5b4eaa8d771c6bea2895c6f3bd38b
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-05-18 15:26:17 +0200

    (scheduler): add time management

--- services/scheduler.go
+++ services/scheduler.go
@@ -1,6 +1,43 @@
 package services
 
+import (
+	"math"
+)
+
 const (
 	OpeningHour int = 9
-	ClosingHour     = 5
+	ClosingHour int = 17
+	TimeToLunch int = 1
 )
+
+var ActivityDurations = map[string]int{
+	"museum":     2,
+	"garden":     1,
+	"illustrium": 2,
+}
+
+const OneDay = 1
+
+func GetFreeTime(numberOfDays int) int {
+	return ((ClosingHour - OpeningHour) - TimeToLunch) * numberOfDays
+}
+
+func GetTotalNumberOfActivities(freeTime int) int {
+	meanTimePerActivity := (ActivityDurations["garden"] + ActivityDurations["museum"] + ActivityDurations["illustrium"]) / 3
+	return freeTime / meanTimePerActivity
+}
+
+func GetAmountOfActivitiesPerDay(totalNumberOfActivities, numberOfDays int) int {
+	amountOfActivitiesPerDay := totalNumberOfActivities / numberOfDays
+	roundedAmountOfActivitiesPerDay := int(math.Floor(float64(amountOfActivitiesPerDay)))
+	return roundedAmountOfActivitiesPerDay
+}
+
+func IsValidScheduleForTheDay(activitiesType []string) bool {
+	totalFreeTime := GetFreeTime(OneDay)
+	totalActivityTime := 0
+	for _, activity := range activitiesType {
+		totalActivityTime += ActivityDurations[activity]
+	}
+	return totalActivityTime <= totalFreeTime
+}
