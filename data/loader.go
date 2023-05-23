cf61c02e8f9dc798119331303de15e5b87672bb9
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-05-23 13:23:36 +0200

    feat: load and query data

--- data/loader.go
+++ data/loader.go
@@ -11,16 +11,27 @@ import (
 
 func LoadSites() {
 	log.Println("====== Creating sites database... ======")
-	csvFile, _ := os.Open("t3-v7.csv")
+	csvFile, err := os.Open("t3-v7.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+
 	reader := csv.NewReader(csvFile)
 	reader.Comma = ';'
 	_, _ = reader.Read()
-	records, _ := reader.ReadAll()
-	if records == nil {
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(records) == 0 {
 		log.Fatal("No records found at: " + csvFile.Name())
 	}
 
-	db, _ := sql.Open("sqlite3", "./t3.db")
+	db, err := sql.Open("sqlite3", "./t3.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer db.Close()
 
 	query := `CREATE TABLE IF NOT EXISTS sites (
@@ -33,29 +44,45 @@ func LoadSites() {
 		city VARCHAR(255) NOT NULL,
 		postcode VARCHAR(255) NOT NULL,
 		department VARCHAR(255) NOT NULL,
-		region VARCHAR(255) NOT NULL,		
+		region VARCHAR(255) NOT NULL,
 		website VARCHAR(255) NOT NULL,
 		description VARCHAR(255) NOT NULL
 	);`
 
-	_, err := db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Default().Println("Sites table created")
 	log.Default().Println("Loading sites...")
-	tx, _ := db.Begin()
 
-	stmt, _ := tx.Prepare("INSERT INTO sites (name, type, lat, lng, street, city, postcode, department, region, website, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO sites (name, type, lat, lng, street, city, postcode, department, region, website, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for _, record := range records {
 		_, err = stmt.Exec(record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8], record[9], record[10])
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error inserting record:", err)
+			rollbackErr := tx.Rollback()
+			if rollbackErr != nil {
+				log.Println("Rollback error:", rollbackErr)
+			}
+			log.Fatal("Transaction rolled back due to error")
 		}
 	}
 
-	tx.Commit()
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Println("Sites loaded")
 	log.Println("====== Sites database created ======")
 }
